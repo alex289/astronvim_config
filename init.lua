a9f3290a944860dcf6bcc43598eb1d81280e9a38
Alex <mccr4fthd@gmail.com>
2023-03-13 18:29:03 +0100

    Update config for astronvim 3

--- init.lua
+++ init.lua
@@ -1,39 +1,41 @@
 return {
-  options = {
-    opt = {
-      cmdheight = 1,
+    options = {
+      opt = {
+        cmdheight = 1,
+      },
+      g = {
+        copilot_no_tab_map = true,
+      },
     },
-    g = {
-      copilot_no_tab_map = true,
+    plugins = {
+      {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require("astronvim.utils.status")
+        opts.statusline = { -- statusline
+          hl = { fg = "fg", bg = "bg" },
+          status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+          status.component.git_branch(),
+          status.component.file_info { filetype = {}, filename = false, file_modified = false },
+          status.component.git_diff(),
+          status.component.diagnostics(),
+          status.component.fill(),
+          status.component.cmd_info(),
+          status.component.fill(),
+          status.component.lsp(),
+          status.component.treesitter(),
+          status.component.nav(),
+          -- remove the 2nd mode indicator on the right
+        }
+
+        -- return the final configuration table
+        return opts
+      end,
     },
-  },
-  plugins = {
-    heirline = function(config)
-      -- the first element of the default configuration table is the statusline
-      config[1] = {
-        -- set the fg/bg of the statusline
-        hl = { fg = "fg", bg = "bg" },
-        -- when adding the mode component, enable the mode text with padding to the left/right of it
-        astronvim.status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
-        -- add all the other components for the statusline
-        astronvim.status.component.git_branch(),
-        astronvim.status.component.file_info(),
-        astronvim.status.component.git_diff(),
-        astronvim.status.component.diagnostics(),
-        astronvim.status.component.fill(),
-        astronvim.status.component.lsp(),
-        astronvim.status.component.treesitter(),
-        astronvim.status.component.nav(),
-      }
-      -- return the final configuration table
-      return config
+    { "github/copilot.vim", lazy = false }
+    },
+    polish = function()
+      local copilot_options = { silent = true, expr = true, script = true }
+      vim.api.nvim_set_keymap("i", "öö", "copilot#Accept(<Tab>)", copilot_options)
     end,
-    init = {
-      { "github/copilot.vim" }
-    }
-  },
-  polish = function()
-    local copilot_options = { silent = true, expr = true, script = true }
-    vim.api.nvim_set_keymap("i", "öö", "copilot#Accept(<Tab>)", copilot_options)
-  end,
-}
+  }
