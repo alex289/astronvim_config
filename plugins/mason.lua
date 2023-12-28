a282c811a63c129dfea2d7a713130e56d8d31dac
alex289 <mccr4fthd@gmail.com>
2023-12-28 19:35:26 +0100

    feat: Ensure lsps are installed

---
+++ plugins/mason.lua
@@ -0,0 +1,30 @@
+return {
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "prismals",
+        "astro",
+        "tsserver",
+        "eslint",
+        "jsonls",
+        "tailwindcss",
+        "yamlls",
+        "csharp_ls",
+        "cssls",
+      })
+    end,
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "prettierd",
+        "biome",
+        "csharpier",
+      })
+    end,
+  },
+}
